6b5f2b1b66fe246d8d3f2d8de2c1da8473c9f86e
Amy Upthagrove <97408960+amy-upthagrove@users.noreply.github.com>
2022-01-19 19:06:24 -0600

    Add files via upload

--- imageGetter/script.js
+++ imageGetter/script.js
@@ -1,20 +1,25 @@
-const unsplashApiKey = "LGYdhFr9OaXxY6jAe4O0t4Dcbk3N8npmfdZbAmZpGmY";
-
 document.getElementById("form").addEventListener("submit", formSubmit);
+
+const unsplashApiKey = "LGYdhFr9OaXxY6jAe4O0t4Dcbk3N8npmfdZbAmZpGmY";
 
 function formSubmit(evt) {
   evt.preventDefault();
 
   const searchBar = document.getElementById("searchBar");
-  const searchVal = searchBar.value;
-  const unsplashApiUrl = `https://api.unsplash.com/photos/?client_id=${unsplashApiKey}`;
+  const searchName = searchBar.value;
 
-  fetch(unsplashApiUrl) 
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (imgDisplay) {
-      var img = document.getElementById("img");
-      img.innerHTML = imgDisplay.img;
-    });
+  let imgApi = new URL(`https://api.unsplash.com/`);
+  imgApi.pathname = `search/photos`;
+  imgApi.search = `query=${searchName}`;
+
+  const imgPara = document.getElementById("img");
+
+  fetch(imgApi, {
+    headers: {
+      Authorization: "Client-ID _NT_bXtvGlvXc89PFyTP2yE7wlOBpWwxceZiSTWCLqs",
+    },
+  })
+    .then((response) => response.json())
+    // .then((data) => console.log(data));
+    .then((data) => imgPara.setAttribute("src", data.results[0].urls.thumb));
 }
